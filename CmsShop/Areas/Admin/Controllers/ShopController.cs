3702b1e0311a311a3b96e9abfd53cdd5d11e8164
mzajac <matzaj101@gmail.com>
2018-10-22 17:34:47 +0200

    add category to the shop

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShop.App_Data;
+using CmsShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public ShopController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult Categories()
+        {
+            return View(_context.Categories.ToArray().OrderBy(x=>x.Sorting).Select(x=>new CategoryViewModel(x)).ToList());
         }
     }
 }
