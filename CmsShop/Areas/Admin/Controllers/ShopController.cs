6ab5970de9cf38f84efeb18d0f4c0fb81bf68753
mzajac <matzaj101@gmail.com>
2018-10-23 11:37:36 +0200

    Add category ajax

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CmsShop.App_Data;
+using CmsShop.Models.Data;
 using CmsShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,29 @@ namespace CmsShop.Areas.Admin.Controllers
         public IActionResult Categories()
         {
             return View(_context.Categories.ToArray().OrderBy(x=>x.Sorting).Select(x=>new CategoryViewModel(x)).ToList());
+        }
+
+        [HttpPost]
+        public string AddNewCategory(string  catName)
+        {
+            string id;
+            if (_context.Categories.Any(x => x.Name == catName))
+            {
+                return "tytulzajety";
+            }
+            CategoryDTO catDto = new CategoryDTO();
+            catDto.Name = catName;
+            catDto.Slug = catName.Replace(" ", "-").ToLower();
+            catDto.Sorting = 1000;
+
+            _context.Categories.Add(catDto);
+            _context.SaveChanges();
+
+
+            id = catDto.Id.ToString();
+
+            return id;
+
         }
     }
 }
